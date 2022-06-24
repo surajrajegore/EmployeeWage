7cc295bd56dc3bc330f676e226238671bbbd42fc
surajrajegore <rajegoresuraj2880@gmail.com>
2022-06-24 18:46:36 +0530

    Added:solved using class and methods

--- src/com/bridgelabz/EmployeeWageComputation.java
+++ src/com/bridgelabz/EmployeeWageComputation.java
@@ -1,18 +1,22 @@
 package com.bridgelabz;
 
 public class EmployeeWageComputation {
-    public static void main(String[] args) {
-        final int FULL_TIME = 1;
-        final int PART_TIME = 2;
-        final int WAGE_PER_HOUR=20;
-        final int MAXI_WORKING_DAYS = 20;
-        final int MAXI_WORKING_HOURS = 100;
-        int totalWage = 0;
-        int workingHours=0;
+    public static void main(String args[])
+    {
+        calculateTotalWage();
+    }
+    public static void calculateTotalWage() {
+        final int PART_TIME = 1;
+        final int FULL_TIME = 2;
+        final int WAGE_PER_HR = 20;
+        final int MAX_WORKING_DAYS = 20;
+        final int MAX_WORKING_HOURS = 100;
 
-        for (int day = 1, totalWorkingHrs = 0; day <= MAXI_WORKING_DAYS
-                && totalWorkingHrs < MAXI_WORKING_HOURS; day++, totalWorkingHrs += workingHours)
-        {
+        int totalWage = 0;
+        int workingHours = 0;
+        for (int days = 1, totalWorkingHours = 0; days <= MAX_WORKING_DAYS
+                && totalWorkingHours < MAX_WORKING_HOURS; days++, totalWorkingHours += workingHours) {
+
             int empType = (int) (Math.random() * 100) % 3;
             switch (empType) {
                 case FULL_TIME:
@@ -22,13 +26,15 @@ public class EmployeeWageComputation {
                     workingHours = 4;
                     break;
                 default:
-                    workingHours=0;
+                    workingHours = 0;
                     break;
             }
-            int wage = workingHours * WAGE_PER_HOUR;
-            totalWage =totalWage + wage;
+            int wage = workingHours * WAGE_PER_HR;
+            totalWage += wage;
+            System.out.println("days " + days+" workingHours " + workingHours + " wage " + wage + " totalWorkingHours " + totalWorkingHours + " workingHours " + workingHours);
 
         }
         System.out.println("Total wage for a month is " + totalWage);
     }
 }
+
