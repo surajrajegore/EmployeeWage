4984b36998691f86e1ac4cdb65e06c472d50325c
surajrajegore <rajegoresuraj2880@gmail.com>
2022-06-24 18:04:52 +0530

    Solved problem using switch case

--- src/com/bridgelabz/EmployeeWageComputation.java
+++ src/com/bridgelabz/EmployeeWageComputation.java
@@ -7,17 +7,19 @@ public class EmployeeWageComputation {
         final int WAGE_PER_HOUR=20;
         int workingHour=0;
         int empType = (int) (Math.random() * 100) % 3;
-        if (empType == FULL_TIME) {
-            System.out.println("Employee is Present with full time");
-            workingHour = 8;
-
-        } else if (empType == PART_TIME){
-            System.out.println("Employee is Present With Part time");
-            workingHour = 4;
-        }else {
-            System.out.println("Employee is Absent");
+        switch (empType) {
+            case FULL_TIME:
+                System.out.println("Employee is Present Full time");
+                workingHour = 8;
+                break;
+            case PART_TIME:
+                System.out.println("Employee is Present Part time");
+                workingHour = 4;
+                break;
+            default:
+                System.out.println("Employee is Absent");
         }
         int wage = workingHour * WAGE_PER_HOUR;
-        System.out.println("Employee Daily wage is "+wage);
+        System.out.printl   n("Employee Daily wage is "+wage);
     }
 }
