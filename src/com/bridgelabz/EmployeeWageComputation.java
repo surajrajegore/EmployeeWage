6a69b77f36a75729bfca7060ac5b3b1cf4b3f371
surajrajegore <rajegoresuraj2880@gmail.com>
2022-06-24 17:22:25 +0530

    Added:code Employee is present or absent

--- src/com/bridgelabz/EmployeeWageComputation.java
+++ src/com/bridgelabz/EmployeeWageComputation.java
@@ -2,6 +2,16 @@ package com.bridgelabz;
 
 public class EmployeeWageComputation {
     public static void main(String[] args){
-        System.out.println("Welcome to Employee Wage Computation");
+        final int FULL_TIME = 1;
+        int empType = (int) (Math.random() * 100) % 2;
+        if (empType == FULL_TIME)
+        {
+            System.out.println("Employee is Present");
+
+        } else
+        {
+            System.out.println("Employee is Absent");
+
+        }
     }
 }
