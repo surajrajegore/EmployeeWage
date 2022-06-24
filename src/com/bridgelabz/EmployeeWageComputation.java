2aaaf69d28263aad7d5913561875f28ae7d7247c
surajrajegore <rajegoresuraj2880@gmail.com>
2022-06-24 17:52:34 +0530

    Added:Calculated wage

--- src/com/bridgelabz/EmployeeWageComputation.java
+++ src/com/bridgelabz/EmployeeWageComputation.java
@@ -3,15 +3,17 @@ package com.bridgelabz;
 public class EmployeeWageComputation {
     public static void main(String[] args){
         final int FULL_TIME = 1;
+        final int WAGE_PER_HOUR=20;
+        int workingHour=0;
         int empType = (int) (Math.random() * 100) % 2;
-        if (empType == FULL_TIME)
-        {
+        if (empType == FULL_TIME) {
             System.out.println("Employee is Present");
+            workingHour = 8;
 
-        } else
-        {
+        } else {
             System.out.println("Employee is Absent");
-
         }
+        int wage = workingHour * WAGE_PER_HOUR;
+        System.out.println("Employee Daily wage is "+wage);
     }
 }
